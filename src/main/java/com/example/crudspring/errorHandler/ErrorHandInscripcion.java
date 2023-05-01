b0177ba44d312ce994fc7cd1884a59437dc47a5a
Jorge Alberto  Morales Gutierrez <L17330479@hermosillo.tecnm.mx>
2023-05-01 15:21:36 -0700

    feat error handler, refactor

---
+++ src/main/java/com/example/crudspring/errorHandler/ErrorHandInscripcion.java
@@ -0,0 +1,2 @@
+package com.example.crudspring.errorHandler;public class ErrorHandInscripcion {
+}
---
+++ src/main/java/com/example/crudspring/errorHandler/ErrorResponseMio.java
@@ -0,0 +1,9 @@
+package com.example.crudspring.errorHandler;
+
+import java.time.LocalTime;
+
+public class ErrorResponse {
+    private int status;
+    private String message;
+    private LocalTime timeStamp;
+}
